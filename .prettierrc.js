15b8ffda0d4b6959bbd66c9449d5a908fb96a70f
Brighten Tompkins <brightenqtompkins@gmail.com>
2022-04-10 19:23:28 -0700

    ci(GitHubCI): added github ci and documentation files

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,5 +1,11 @@
 module.exports = {
   arrowParens: 'avoid',
+  printWidth: 80,
+  semi: true,
+  singleQuote: true,
+  tabWidth: 2,
+  endOfLine: 'auto',
+  useTabs: false,
   bracketSameLine: true,
   bracketSpacing: false,
   singleQuote: true,
